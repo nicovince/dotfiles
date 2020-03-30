51fa35d9b40c7363c285c9e738cd77a0cb8e26e4
Nicolas VINCENT <nicolas.vincent@vossloh.com>
2020-03-30 12:00:34 +0200

    Move screenrc to own folder, and add init script
    Do not rely on external script (bin/) anymore
    init_config.sh copied and modfied from :
    https://github.com/marinacr/configuration_files/blob/master/config_files_script

---
+++ README.md
@@ -0,0 +1,3 @@
+Dotfile repository used to store all configuration files.  
+
+The configuration files read by tools are symbolic link pointing to those files. If the links do not exist yet, they can be automatically created with `init\_config.sh`
---
+++ init_config.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Setup configurations using symbolic link from home folder to config files
+# within this repository.
+# Script copied and modified from :
+# https://github.com/marinacr/configuration_files/blob/master/config_files_script
+
+config_dir=$(git rev-parse --show-toplevel)
+ignore_patterns=('README.md' "$(basename $0)" '.git$' '~' 'backup' 'screenrc_layouts' '.*\.swp' 'gonzalez_config' 'revit.sh' 'git_prompt.zsh')
+backup_folder=${config_dir}_$(date "+%F_%H.%M.%S")
+
+#symbolic link creation
+for file in `ls -A $config_dir`
+do
+    ignore_flag=0
+    for p in ${ignore_patterns[*]}; do
+        match=$(echo $file | grep -c "${p}")
+        if [ $match -ne 0 ]; then
+            ignore_flag=1
+        fi
+    done
+    if [ $ignore_flag -ne 1 ]
+    then
+        if [ -e $HOME/$file -a ! -h $HOME/$file ]
+        then
+            #backup existing conf file
+            mkdir -p ${backup_folder}
+            echo "mv $HOME/${file} ${backup_folder}/${file}.old"
+        fi
+        if [ ! -h ${HOME}/${file} ]; then
+            echo ln -s $config_dir/$file $HOME/$file
+        fi
+
+    fi
+done
+
+#git configuration
+read -p 'Git name : ' name
+read -p 'Git mail : ' mail
+cat <<EOF > $HOME/.gitconfig_user
+[user]
+        name = ${name}
+        email = ${mail}
+EOF

f06320506cb379065c456bee9376d5999f817d25
Nicolas VINCENT <nicolas.vincent@vossloh.com>
2023-06-23 09:00:02 +0200

    anyr: tmux sessions
    zephyr tabs
    esp32 tabs
    stm32 tabs

--- tmux_sessions.sh
+++ tmux_sessions.sh
@@ -208,3 +208,41 @@ tmux_r4ip()
 
     tmux attach-session -d -t ${SESSION_NAME}
 }
+
+ANYR_WORKSPACE="/home/nicolas/work/siema/be/BT-GSMR"
+tmux_anyr_zeph()
+{
+    SESSION_NAME="anyr"
+    tmux attach-session -d -t ${SESSION_NAME}
+    ret="$?"
+    if [[ "${ret}" == 0 ]]; then
+        return
+    fi
+    tmux new-session -d -s ${SESSION_NAME}
+    tmux rename-window "west"
+    tmux send-keys "cd ${ANYR_WORKSPACE}/anyr-zephyr" C-m
+    tmux send-keys "get_stm32" C-m
+    tmux send-keys "workon anyr-zephyr" C-m
+
+    tmux new-window -n "vim.anyr"
+    tmux send-keys "cd ${ANYR_WORKSPACE}/anyr-zephyr" C-m
+
+    tmux new-window -n "git.anyr"
+    tmux send-keys "cd ${ANYR_WORKSPACE}/anyr-zephyr/anyr-zephyr" C-m
+    tmux send-keys "get_gh_completion" C-m
+
+    tmux new-window -n "vim.esp32"
+    tmux send-keys "cd ${ANYR_WORKSPACE}/anyr-esp32/anyr" C-m
+
+    tmux new-window -n "git.esp32"
+    tmux send-keys "cd ${ANYR_WORKSPACE}/anyr-esp32/anyr" C-m
+    tmux send-keys "workon anyr" C-m
+    tmux send-keys "source env.sh /home/nicolas/work/siema/vendors/espressif/xtensa-esp32-elf" C-m
+
+    tmux new-window -n "vim.mitm"
+    tmux send-keys "cd ${ANYR_WORKSPACE}/anyr-i2s-mitm" C-m
+
+    tmux new-window -n "git.mitm"
+    tmux send-keys "cd ${ANYR_WORKSPACE}/anyr-i2s-mitm" C-m
+    tmux send-keys "get_gh_completion" C-m
+}
